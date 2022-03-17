06f459ff0b4a0b12b4ea8ee8bbd273a27546a60e
Lishinski <testerqa.lesh@gmail.com>
2022-03-17 12:51:36 +0200

    commit menu

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,48 +1,46 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class LoginTests extends TestBase {
 
+
     @Test
     public void loginSuccess() {
-//open login form
-        WebElement loginItem = wd.findElement(By.cssSelector("[href='/login']"));
-        loginItem.click();
-//fill email
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("testerqa.lesh@gmail.com");
-//fill password
-        WebElement passwordInput = wd.findElement(By.xpath("//input[2]"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("Qa32$2021");
-//click login
-        wd.findElement(By.xpath("//*[text()=' Login']")).click();
-        Assert.assertTrue(wd.findElements(By.xpath("//*[text()='Sign Out']")).size() > 0);
+        //open login form
+//        WebElement loginItem = wd.findElement(By.cssSelector("[href='/login']"));
+//        loginItem.click();
+//        //fill email
+//        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys("noa@gmail.com");
+//
+//        // fill password
+//        WebElement passwordInput = wd.findElement(By.xpath("//input[2]"));
+//        passwordInput.click();
+//        passwordInput.clear();
+//        passwordInput.sendKeys("Nnoa12345$");
+//        //click button Login
+//        wd.findElement(By.xpath("//*[text()=' Login']")).click();
+//
+//
+//        Assert.assertTrue(wd.findElements(By.xpath("//*[text()='Sign Out']")).size() > 0);
+
 
     }
 
     @Test
-    public void loginSuccessNew()
-    {
-        //open login form
-        click(By.cssSelector("[href='/login']"));
-        //fill email
-        type(By.xpath("//input[1]"),"testerqa.lesh@gmail.com");
-        type(By.xpath("//input[2]"),"Qa32$2021");
-        click(By.xpath("//*[text()=' Login']"));
+    public void loginSuccessNew(){
 
-         Assert.assertTrue(isElementPresent(By.xpath("//*[text()='Sign Out']")));
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistartionForm("noa@gmail.com","Nnoa12345$");
+        app.getHelperUser().submitLogin();
 
-
-
-
+        Assert.assertTrue(app.getHelperUser().isLoginRegistrationSuccess());
     }
+
 }
