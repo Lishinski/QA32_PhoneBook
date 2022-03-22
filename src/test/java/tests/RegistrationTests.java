68e4a5215865eb2d2ef6ef689cc0704773087b20
Lishinski <testerqa.lesh@gmail.com>
2022-03-22 21:23:27 +0200

    added before

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,17 +1,24 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase{
 
-
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isSignOutPresent()){
+            app.getHelperUser().signOut();
+        }
+    }
 
     @Test
     public void registrationSuccess(){
 
         int index = (int)(System.currentTimeMillis()/1000)%3600;
-        System.out.println(index);
+        System.out.println("wick"+index+"@gmail.com");
+
 
         app.getHelperUser().openLoginRegistrationForm();
         app.getHelperUser().fillLoginRegistartionForm("wick"+index+"@gmail.com","Ww12345$");
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,18 +6,17 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected  static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager();
 
 
     @BeforeSuite
-    public void setUp(){
+    public void setUp() {
         app.init();
 
     }
 
     @AfterSuite
-    public void tearDown(){
+    public void tearDown() {
         app.stop();
     }
-
 }
