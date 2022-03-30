8aa2cd83f4fecba24deae89daddf3f0c9d688b93
Lishinski <testerqa.lesh@gmail.com>
2022-03-30 12:19:00 +0300

    Commit2903

---
+++ src/test/java/tests/AddNewContactTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import models.Contact;
+import models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddNewContactTest extends TestBase{
+
+
+    @BeforeMethod
+    public void preCondition(){
+        if(!app.getHelperUser().isSignOutPresent()){
+            app.getHelperUser().login(new User().withEmail("noa@gmail.com").withPassword("Nnoa12345$"));
+        }
+    }
+
+    @Test
+    public void addNewContactSuccess(){
+        int index = (int) (System.currentTimeMillis()/1000)%3600;
+        Contact contact = Contact.builder()
+                .name("John"+index)
+                .lastName("Dow")
+                .phone("1234567"+index)
+                .email("john"+index+"@mail.com")
+                .address("Rehovot")
+                .description("The best friend").build();
+        System.out.println(contact.getName());
+        System.out.println(contact.getPhone());
+
+        app.contact().openContactForm();
+        app.contact().fillContactForm(contact);
+        app.contact().saveContact();
+
+        Assert.assertTrue(app.contact().isContactByName(contact.getName()));
+        Assert.assertTrue(app.contact().isContactByPhone(contact.getPhone()));
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,17 +6,18 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager();
+    protected  static ApplicationManager app = new ApplicationManager();
 
 
     @BeforeSuite
-    public void setUp() {
+    public void setUp(){
         app.init();
 
     }
 
     @AfterSuite
-    public void tearDown() {
-        app.stop();
+    public void tearDown(){
+        //app.stop();
     }
+
 }
